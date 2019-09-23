968e806dc45baeb435e0d9dbdaf52a3fe7ad840b
zhangyurui <zhangyurui@beisen.com>
2019-09-23 10:27:12 +0800

    feat(route): 添加单页面router

--- src/pages/Topics/index.js
+++ src/pages/Topics/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
-export function Topics() {
-  return <h1>This is Topics Pages</h1>
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+export function Topics({ match }) {
+  return <Router>
+      <div>
+        <h1>topics</h1>
+        <ul>
+          <li>
+            <Link to={`${match.url}/sectionOne`}>One</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/sectionTwo`}>Two</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/sectionThree`}>Three</Link>
+          </li>
+          <Route
+            path={`${match.path}/:id`}
+            component={Topic}
+          />
+          <Route 
+            exact 
+            path={`${match.path}`} 
+            render={() =>{
+            return <h2>select a topic</h2>}
+            } />
+        </ul>
+      </div>
+    </Router> 
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.id}</h3>
+    </div>
+  );
 }
 export function specificTopics(props) {
   return <div>
